962a6ad8f15f6d62f86fe8d9a6c2412d40234587
Muhammad Aslam <muhammad.aslam@speridian.com>
2025-04-29 14:49:23 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rio.Coding.Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
